2d4b7092a93f80e5b899d29bc4311dba651bbf69
JakobDevelops <jakobwiemer@web.de>
2019-05-28 12:55:50 +0800

    Initial Commit

---
+++ CheckWise2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckWise2
+//
+//  Created by Jakob Wiemer on 28.05.19.
+//  Copyright © 2019 Jakob Wiemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CheckWise2Tests/CheckWise2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CheckWise2Tests.swift
+//  CheckWise2Tests
+//
+//  Created by Jakob Wiemer on 28.05.19.
+//  Copyright © 2019 Jakob Wiemer. All rights reserved.
+//
+
+import XCTest
+@testable import CheckWise2
+
+class CheckWise2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
