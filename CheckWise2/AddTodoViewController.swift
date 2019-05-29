5ad776f94918adaeae3a497144c0eef29b4d6335
JakobDevelops <jakobwiemer@web.de>
2019-05-29 12:07:32 +0800

    CheckWise2

---
+++ CheckWise2/AddTodoViewController.swift
@@ -0,0 +1,81 @@
+//
+//  AddTodoViewController.swift
+//  checkwise
+//
+//  Created by Jakob Wiemer on 08.05.19.
+//  Copyright © 2019 Jakob Wiemer. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AddTodoViewController: UIViewController {
+    
+    var managedContext: NSManagedObjectContext!
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(with:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        
+        textView.becomeFirstResponder() //Anweisung, dass die TextView direkt ausgewählt ist beim öffnen der Aktivität
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    //Methode um den Bottom Constraint anzupassen, sobald das Keyboard sich öffnet
+    @objc func keyboardWillShow(with notification: Notification){
+        
+        let key = "UIKeyboardFrameEndUserInfoKey"
+        guard let keyboardFrame = notification.userInfo?[key] as? NSValue else {return}
+        
+        let keyboardHeight = keyboardFrame.cgRectValue.height + 7
+        
+        bottomConstraint.constant = keyboardHeight
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    @IBAction func cancel(_ sender: UIButton) {
+        dismiss(animated: true)
+        textView.resignFirstResponder()
+        //wenn cancel gedrückt wird, wird das Keyboard instant geschlossen
+    }
+    
+    @IBAction func done(_ sender: UIButton) {
+        guard let text = textView.text else {
+            print("Bitte Text eingeben")
+            return
+        }
+        CoreDataManager.shared.createObj(name: text)
+        
+        dismiss(animated: true)
+    }
+    
+}
+
+extension AddTodoViewController: UITextViewDelegate{
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        if doneButton.isHidden{
+            textView.text.removeAll() //sobald remove gedrückt wird, wird der gesamte Text entfernt
+            textView.textColor = .gray //textfarbe stat orange jetzt auf grau
+            
+            doneButton.isHidden = false
+            UIView.animate(withDuration: 0.3) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+}
---
+++ CheckWise2/CoreDataManager.swift
@@ -0,0 +1,65 @@
+//
+//  CoreDataManager.swift
+//  CheckWise2
+//
+//  Created by Jakob Wiemer on 29.05.19.
+//  Copyright © 2019 Jakob Wiemer. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    var todos = [Todo]()
+    
+    // Cotnext als Variable
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext //Zugriff auf App-Delegate
+    
+    private init(){
+        loadItems()
+    }
+    
+    //Objekte erstellen
+    func createObj(name: String) {
+        let todo = NSEntityDescription.insertNewObject(forEntityName: "Todo", into: context) as! Todo
+        //Objekte werden aus der Entitiy Todo erstellt und im Kontext abgespeichert
+        
+            todo.name = name
+            todo.completed = false
+        
+            safeContext()
+            print("Objekt erstellt und gesichert.")
+    }
+    
+    //Count Array
+    func getNumberOfItem() -> Int {
+        return todos.count
+    }
+    
+    func getTodoItem(index: Int) -> Todo {
+        return todos[index]
+    }
+    
+    //Objekte laden
+    func loadItems(){
+        let request = NSFetchRequest<Todo>(entityName: "Todo")
+        
+        do {
+            todos = try context.fetch(request)
+            print("Geladen")
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    //Context speicher: Objekte werden gespeichert
+    func safeContext(){
+        do {
+            try context.save()
+        } catch  {
+            print(error.localizedDescription)
+        }
+    }
+}
